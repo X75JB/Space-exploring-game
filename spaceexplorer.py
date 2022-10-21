d956e029664899fbb5843aecbcbeece61dfa6d9f
Jackson Blackman <80335379+X75JB@users.noreply.github.com>
2022-10-21 13:26:39 -0400

    Update spaceexplorer.py

--- spaceexplorer.py
+++ spaceexplorer.py
@@ -17,12 +17,13 @@ def slowprint(str):
     typing_speed = random.uniform(.001,.15)
     sleep(typing_speed)
 
-# intro
+# Intro
 slowprint('The year is 2054 and the true first man-operated mission to mars is taking place. \n')
 skip = str(input('\n PRESS ENTER TO CONTINUE \n'))
 os.system('cls')
 
-slowprint('Only some of the most fit personal were chosen in order to ensure survival. Your name is: \n')
+## Name collection
+slowprint('Only some of the most fit personnel were chosen in order to ensure survival. Your name is: \n')
 first_name = str(input('First name: '))
 last_name = str(input('Last name: '))
 skip = str(input('\n PRESS ENTER TO CONTINUE \n'))
@@ -36,3 +37,81 @@ slowprint('Although this mission was well engineered you could have not expect e
 print()
 skip = str(input('\n PRESS ENTER TO CONTINUE \n'))
 os.system('cls')
+
+# Spaceship animation
+print(r'''
+ /\/\/\                            /  \
+| \  / |                         /      \
+|  \/  |                       /          \
+|  /\  |----------------------|     /\     |
+| /  \ |                      |    /  \    |
+|/    \|                      |   /    \   |
+|\    /|                      |  | (  ) |  |
+| \  / |                      |  | (  ) |  |
+|  \/  |                 /\   |  |      |  |   /\
+|  /\  |                /  \  |  |      |  |  /  \
+| /  \ |               |----| |  |      |  | |----|
+|/    \|---------------|    | | /|   .  |\ | |    |
+|\    /|               |    | /  |   .  |  \ |    |
+| \  / |               |    /    |   .  |    \    |
+|  \/  |               |  /      |   .  |      \  |
+|  /\  |---------------|/        |   .  |        \|
+| /  \ |              /   NASA   |   .  |  NASA    \
+|/    \|              (          |      |           )
+|/\/\/\|               |    | |--|      |--| |    |
+------------------------/  \-----/  \/  \-----/  \--------
+                        \\//     \\//\\//     \\//
+                         \/       \/  \/       \/
+                         ''')
+sleep(.5)
+print(r'''                        \\//     \\//\\//     \\//
+                         \/       \/  \/       \/''')
+print()
+sleep(.5)
+print(r'''                        \\//     \\//\\//     \\//
+                         \/       \/  \/       \/''')
+print()
+sleep(.5)
+print(r'''                        \\//     \\//\\//     \\//
+                         \/       \/  \/       \/''')
+print()
+sleep(.5)
+print(r'''                        \\//     \\//\\//     \\//
+                         \/       \/  \/       \/''')
+print()
+sleep(.5)
+print(r'''                        \\//     \\//\\//     \\//
+                         \/       \/  \/       \/''')
+skip = str(input('\n PRESS ENTER TO CONTINUE \n'))
+os.system('cls')
+
+# Outer space animation
+print(r'''
+                   .'.
+                   |o|
+                  .'o'.
+                  |.-.|
+                  '   '
+                   ( )
+                    )
+                   ( )
+
+               ____
+          .-'""p 8o""`-.
+       .-'8888P'Y.`Y[ ' `-.
+     ,']88888b.J8oo_      '`.
+   ,' ,88888888888["        Y`.
+  /   8888888888P            Y8\
+ /    Y8888888P'             ]88\
+:     `Y88'   P              `888:
+:       Y8.oP '- >            Y88:
+|          `Yb  __             `'|
+:            `'d8888bo.          :
+:             d88888888ooo.      ;
+ \            Y88888888888P     /
+  \            `Y88888888P     /
+   `.            d88888P'    ,'
+     `.          888PP'    ,'
+       `-.      d8P'    ,-'   
+          `-.,,_'__,,.-'
+          ''')
